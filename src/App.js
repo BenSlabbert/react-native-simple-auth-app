97a0b18705288469d2ece04237e984758c6cbc21
Ben Slabbert <ben@hex.co.za>
2017-12-28 20:13:30 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { Header } from './components/common'
+
+class App extends Component {
+
+    render() {
+        return (
+            <Header title='AuthApp'/>
+        );
+    }
+}
+
+export default App;
