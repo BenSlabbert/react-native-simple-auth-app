c527c7f3c1b88d77b3b88128113b1b1705f63259
Ben Slabbert <ben@hex.co.za>
2017-12-29 14:00:11 +0200

    adding login form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common'
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
     componentWillMount() {
-        firebase.initializeApp({
+        firebase.initializeApp( {
             apiKey: 'AIzaSyDOh-lqZW_5HP0oJQ7jx5oERBf0gGCDUPw',
             authDomain: 'authapp-ben.firebaseapp.com',
             databaseURL: 'https://authapp-ben.firebaseio.com',
             projectId: 'authapp-ben',
             storageBucket: 'authapp-ben.appspot.com',
             messagingSenderId: '785510905356'
-        });
+        } );
     }
 
     render() {
         return (
             <View>
                 <Header title='AuthApp'/>
-                <Text>auth app text</Text>
+                <LoginForm/>
             </View>
 
         );
