8ce2e1c34bedbfab2af66f010c6eba238ff70291
Ben Slabbert <ben@hex.co.za>
2017-12-29 12:54:31 +0200

    adding firebase addinig firebase initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common'
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDOh-lqZW_5HP0oJQ7jx5oERBf0gGCDUPw',
+            authDomain: 'authapp-ben.firebaseapp.com',
+            databaseURL: 'https://authapp-ben.firebaseio.com',
+            projectId: 'authapp-ben',
+            storageBucket: 'authapp-ben.appspot.com',
+            messagingSenderId: '785510905356'
+        });
+    }
 
     render() {
         return (
